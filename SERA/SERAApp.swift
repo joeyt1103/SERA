a48590e76297d64fa0951c629e0a5f1a240dc889
joeyt1103 <35017092+joeyt1103@users.noreply.github.com>
2025-02-05 11:09:23 -0500

    Initial Commit

---
+++ SERA/SERAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SERAApp.swift
+//  SERA
+//
+//  Created by Joey Trave on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SERAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SERATests/SERATests.swift
@@ -0,0 +1,17 @@
+//
+//  SERATests.swift
+//  SERATests
+//
+//  Created by Joey Trave on 2/5/25.
+//
+
+import Testing
+@testable import SERA
+
+struct SERATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
