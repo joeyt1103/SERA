451d397f0caffdfb5a39b39557ebc08c1baeb7a8
joeyt1103 <35017092+joeyt1103@users.noreply.github.com>
2025-02-10 11:18:59 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- SERA/SERAApp.swift
+++ SERA/SERAApp.swift
@@ -1,17 +1,32 @@
 //
-//  SERAApp.swift
-//  SERA
+// SAFEApp.swift
+// SAFE
 //
-//  Created by Joey Trave on 2/5/25.
+// Created by Kevin Gualano on 9/18/24.
 //
-
 import SwiftUI
-
 @main
-struct SERAApp: App {
+struct SAFE: App {
+    @StateObject private var userState = UserState.shared
+    @AppStorage("isAuthenticated") private var isAuthenticated = false // Use @AppStorage to
+bind directly to UserDefaults
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if isAuthenticated {
+                ContentView(isAuthenticated: $isAuthenticated)
+                //RequirementsView().environmentObject(UserState.shared)// Show ContentView if
+authenticated
+            } else {
+                Login(isAuthenticated: $isAuthenticated) // Show Login view if not
+authenticated
+            }
         }
     }
+}
+    // Simple loading view
+    struct LoadingView: View {
+        var body: some View {
+            ProgressView("Loading...")
+                .progressViewStyle(CircularProgressViewStyle())
+        }
 }
